05cd5f4ff6df082635cebe3958d13ee25c445117
Shingo Kawamura <36791900+kawamurashingo@users.noreply.github.com>
2026-09-22 18:49:23 +0900

    release: bump version to 1.02

--- lib/HTTP/API/Core.pm
+++ lib/HTTP/API/Core.pm
@@ -12,7 +12,7 @@ use HTTP::API::Core::Response;
 use HTTP::API::Core::Error;
 use HTTP::API::Core::Pagination;
 
-our $VERSION = '1.01';
+our $VERSION = '1.02';
 
 sub new {
     my ($class, %args) = @_;
