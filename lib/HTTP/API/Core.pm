856c99e33aad99e81bdf55722da08be0604f4e91
Shingo Kawamura <36791900+kawamurashingo@users.noreply.github.com>
2026-09-23 20:37:04 +0900

    Bump version to 1.07

--- lib/HTTP/API/Core.pm
+++ lib/HTTP/API/Core.pm
@@ -12,7 +12,7 @@ use HTTP::API::Core::Response;
 use HTTP::API::Core::Error;
 use HTTP::API::Core::Pagination;
 
-our $VERSION = '1.06';
+our $VERSION = '1.07';
 
 sub new {
     my ($class, %args) = @_;
