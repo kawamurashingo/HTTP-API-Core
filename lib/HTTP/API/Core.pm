126d2342c984697675ab7c38b79ed3dc1867517e
Shingo Kawamura <36791900+kawamurashingo@users.noreply.github.com>
2026-09-24 23:07:03 +0900

    Reject non-scalar retry method entries

--- lib/HTTP/API/Core.pm
+++ lib/HTTP/API/Core.pm
@@ -420,9 +420,9 @@ sub _normalize_retry {
     die "retry methods must be an array reference\n"
         if ref($methods) ne 'ARRAY';
 
-    my @methods = map { uc($_ // '') } @$methods;
-    die "retry methods must not contain empty values\n"
-        if grep { $_ eq '' } @methods;
+    die "retry methods must contain only non-empty scalars\n"
+        if grep { !defined($_) || ref($_) || $_ eq '' } @$methods;
+    my @methods = map { uc($_) } @$methods;
 
     die "unknown retry option: $_\n" for sort keys %copy;
 
