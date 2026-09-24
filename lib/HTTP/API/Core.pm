43ebf609f0013c0de0e537be5c7cc242cbbc1b87
Shingo Kawamura <36791900+kawamurashingo@users.noreply.github.com>
2026-09-24 23:09:05 +0900

    Preserve retry empty-value validation contract

--- lib/HTTP/API/Core.pm
+++ lib/HTTP/API/Core.pm
@@ -420,8 +420,10 @@ sub _normalize_retry {
     die "retry methods must be an array reference\n"
         if ref($methods) ne 'ARRAY';
 
-    die "retry methods must contain only non-empty scalars\n"
-        if grep { !defined($_) || ref($_) || $_ eq '' } @$methods;
+    die "retry methods must not contain empty values\n"
+        if grep { !defined($_) || (!ref($_) && $_ eq '') } @$methods;
+    die "retry methods must contain only scalar values\n"
+        if grep { ref($_) } @$methods;
     my @methods = map { uc($_) } @$methods;
 
     die "unknown retry option: $_\n" for sort keys %copy;
